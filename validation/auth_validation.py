931cb3441a579a3e593c3e98780f87248a673491
abayomi ogunnusi <abayomiogunnusi@gmail.com>
2023-05-19 10:13:49 +0100

    Chores: Added validation

--- validation/auth_validation.py
+++ validation/auth_validation.py
@@ -1,10 +1,16 @@
 # Define a schema for signup validation
 from marshmallow import Schema, fields, ValidationError
 
+
 class SignupSchema(Schema):
-    username = fields.String(required=True)
+    username = fields.String(
+        required=True, validate=fields.validate.Length(min=3, max=20)
+    )
     email = fields.Email(required=True)
-    password = fields.String(required=True)
+    password = fields.String(
+        required=True, validate=fields.validate.Length(min=6, max=20)
+    )
+
 
 class loginSchema(Schema):
     email = fields.Email(required=True)
